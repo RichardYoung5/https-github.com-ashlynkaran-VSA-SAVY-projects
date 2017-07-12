c4328a60845640b0bfacf3674dd4903a685f70a0
RichardYoung5 <rbyoung.ty@gmail.com>
2017-07-12 11:26:26 -0500

    Add files via upload

--- proj03/proj03.py
+++ proj03/proj03.py
@@ -1,5 +1,5 @@
-# Name:
-# Date:
+# Name: Collin France Richard Young
+# Date:7-12-17
 
 
 """ 
@@ -11,5 +11,30 @@ or exactly right. Keep the game going until the user types exit.
 Keep track of how many guesses the user has taken, and when the game ends, print this out.
 
 """
+#counter
+#loops
+#if else elif
+#loop control variable
+#number generator
+#math operations < > / + = -
+import random
+cow = 0
+X = random.randint(1,9)
+loop_control = True
+print "The number is between 1 and 9. "
+while loop_control == True and cow < 3:
+    guess = int(raw_input("guess the number: "))
+    cow = cow + 1
+    if guess < X:
+        print "Guess is to low."
 
+    elif guess > X:
+        print "Guess is to high."
 
+    else:
+        print "Congrats, you guessed the number!"
+        loop_control =False
+    if cow==3:
+        print"you have run out of chances"
+
+print "it took you ",(cow),"guesses."

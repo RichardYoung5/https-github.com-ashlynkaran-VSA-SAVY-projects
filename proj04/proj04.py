c0cbf75820e99c6a69b258559f05f690451db671
RichardYoung5 <rbyoung.ty@gmail.com>
2017-07-12 15:22:04 -0500

    Add files via upload

--- proj04/proj04.py
+++ proj04/proj04.py
@@ -6,8 +6,9 @@ fddddffd = raw_input("Press ENTER")
 print ""
 
 str = raw_input("Enter a word or phrase: ")
-if str[0] == str[-1]:
-    print "\n Yes! It is true!"
-list = str
-str_list = str.split(" ")
-    for letter in str:
+list1 = []
+for letter in str:
+    list1.append(letter)
+print list1
+
+

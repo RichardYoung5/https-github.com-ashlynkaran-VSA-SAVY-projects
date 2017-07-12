eb313e7e62929fc302881b5b8315541051a0388f
RichardYoung5 <rbyoung.ty@gmail.com>
2017-07-12 09:17:36 -0500

    Add files via upload

--- proj02/proj02_02.py
+++ proj02/proj02_02.py
@@ -1,5 +1,5 @@
-# Name:
-# Date:
+# Name:Richard Young
+# Date:7-11-17
 
 # proj02_02: Fibonaci Sequence
 
@@ -9,5 +9,14 @@ sequence is a sequence of numbers where the next number in the sequence is the s
 previous two numbers in the sequence. The sequence looks like this: 
 1, 1, 2, 3, 5, 8, 13...
 """
-
-
+number =int(raw_input("how many Fibonaci numbers would you like?: "))
+loop_control = 0
+Fibo = 1
+naci = 1
+while loop_control <number:
+    loop_control = loop_control + 1
+    print naci
+    Z = naci + Fibo
+    Fibo = naci
+    naci = Z
+loop_control = False
